ebb4c496712490574a188de392970568826da755
tulloch022 <90489018+tulloch022@users.noreply.github.com>
2025-01-16 09:32:00 -0800

    modified base in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/marathoner/',
   plugins: [react()],
   assetsInclude: ['src/assets/IMG_0437.JPG']
 })
